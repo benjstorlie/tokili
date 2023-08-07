eff522b08246e0969decadf2025ff8ad0bc23839
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-07 13:19:08 -0700

    set up menu

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,10 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { CardGroup, Button, ButtonGroup} from 'react-bootstrap';
+import axios from 'axios';
+import { Button, ButtonGroup, Row, Col} from 'react-bootstrap';
+import MenuBoardElement from '../components/MenuBoardElement'
 
 const Menu = () => {
+  const [menuIsLoading, setMenuIsLoading] = useState(false);
+  const [getMenuError, setGetMenuError] = useState(null);
+  const [boards, setBoards] = useState([]);
+
+  useEffect( async () => {
+    setMenuIsLoading(true);
+    // Fetch initial data from the server
+    try {
+      const response = await axios.get(`api/boards/`);
+      const boardData = response.data;
+
+      setBoards(boardData);
+
+      setMenuIsLoading(false)
+    } catch (error) {
+      setGetMenuError(error);
+      setMenuIsLoading(false);
+      console.error('Error loading board:', error);
+    }
+  }, []);
 
   return (
-    <Button variant="primary"/>
+    <>
+    <Row>
+      <Col sm={12} md={2}>
+        <div>nav element</div>
+      </Col>
+      <Col> 
+        <Row>
+          {boards.map(board => (
+            <Col>
+              <MenuBoardElement key={board.id} boardData={board} />
+            </Col>
+          ))}
+          {menuIsLoading ? (<Col sm={12}><span>Menu Loading...</span></Col>): (<></>)}
+          {getMenuError ? (<Col sm={12}><span>Menu Loading Error</span></Col>): (<></>)}
+        </Row>
+      </Col>
+   
+    </Row>
+    </>
   )
 }
 
