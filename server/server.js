6d40c922baf772cfa196b42ea72531eb2c114ea3
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-07 13:17:55 -0700

    delete handlebars, add react router

--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const PORT = process.env.PORT || 5000;  // 5000 matches the "proxy" address in the client package.json
 
 const sess = {
   secret: 'Super secret secret',
   cookie: {
     maxAge: 3000000,
-    httpOnly: true,
+    //httpOnly: true,
     secure: false,
     sameSite: 'strict',
   },
@@ -31,15 +26,18 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+// Handle other requests by serving the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// For any request that doesn't match an API route, serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
