75448a365e1a876028a0a05286388125dfb0cc19
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-07 13:19:32 -0700

    grid setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Routes } from 'react-router-dom'
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col} from 'react-bootstrap';
 import  Menu  from './pages/Menu'
 import  Editor  from './pages/Editor'
 import  Deploy  from './pages/Deploy'
@@ -9,15 +9,21 @@ import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
-    <div className='container'>
-      <Link to='/login'><Button variant='primary'>Login</Button></Link>
-      <Routes>
-        <Route path='/' element={ <Menu />} />
-        <Route path='/editor' element={ <Editor />} />
-        <Route path='/deploy' element={ <Deploy />} />
-        <Route path='/login' element={ <AuthPage />} />
-      </Routes>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col sm={12}>
+          <Link to='/login'><Button variant='primary'>Login</Button></Link>
+        </Col>
+        <Col>
+          <Routes>
+            <Route path='/' element={ <Menu />} />
+            <Route path='/editor' element={ <Editor />} />
+            <Route path='/deploy' element={ <Deploy />} />
+            <Route path='/login' element={ <AuthPage />} />
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
